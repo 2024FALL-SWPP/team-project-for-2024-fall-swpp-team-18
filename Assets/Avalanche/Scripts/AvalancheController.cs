ab2a3bcfc2471ad4622c3df47a6130bf6d22e276
송원영 <rachelsong10@snu.ac.kr>
2024-11-03 14:15:05 +0900

    add Avalanche asset

---
+++ Assets/Avalanche/Scripts/AvalancheController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheController : MonoBehaviour
+{
+    [SerializeField] public float speed = 4f;
+    [SerializeField] public int level = 1;
+    private float diminishingTime = 10f;
+    private float diminishingRate = 0.05f;
+    public ParticleSystem fog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartAvalanche();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartAvalanche() {
+        speed += (float)level * 2f;
+        StartCoroutine(AvalancheSlide());
+    }
+
+    private IEnumerator AvalancheSlide() {
+        float time = 0;
+        while (true) {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            time += Time.deltaTime;
+            //if (time > 4f) { 
+            //    StopAvalanche();
+            //}
+            yield return null;
+        }
+    }
+
+    public void StopAvalanche() {
+        StartCoroutine(StopAvalancheCoroutine());
+    }
+
+    private IEnumerator StopAvalancheCoroutine() {
+        float time = 0;
+        ParticleSystem.EmissionModule emissionModule = fog.emission;
+        float currentRate = emissionModule.rateOverTime.constant;
+        while (time < diminishingTime) {
+            time += Time.deltaTime;
+            if (currentRate > 0) {
+                emissionModule.rateOverTime = currentRate - diminishingRate * Time.deltaTime;
+            }
+            gameObject.transform.position -= new Vector3(0, diminishingRate * Time.deltaTime, 0);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
