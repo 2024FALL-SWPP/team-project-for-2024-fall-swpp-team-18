e3a0cf3b4621adc4273398640a123f8c7a83e149
rhwodjs0518 <rhwodjs0518@snu.ac.kr>
2024-11-16 12:14:00 +0900

    Implement player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,9 +20,9 @@ public class PlayerController : MonoBehaviour
         //tiltAngle = Quaternion.Lerp(transform.parent.rotation, Quaternion.Euler(new Vector3(0,0,45)), 0.3f);    
         Quaternion tmp = transform.parent.rotation;
         if (Input.GetKey(KeyCode.Q)) {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0,0,-15))*tmp, speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.parent.forward * 15)*tmp, speed * Time.deltaTime);
         } else if (Input.GetKey(KeyCode.E)) {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0,0,15))*tmp, speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.parent.forward * -15)*tmp, speed * Time.deltaTime);
         } else {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, tmp, 100 * Time.deltaTime);
         }
--- Assets/Scripts/PlayerPositionController.cs
+++ Assets/Scripts/PlayerPositionController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerPositionController : MonoBehaviour
 {
     private float Speed = 10.0f; 
-    public Vector3 show;
+
+    private bool BumpWallLeft = false;
+    private bool BumpWallRight = false;
+    private Vector3 CurForward; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +18,36 @@ public class PlayerPositionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        show = transform.forward;  
         transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Q)) {
+
+        if (Input.GetKey(KeyCode.Q) && !BumpWallLeft) {
             transform.Translate (-Vector3.right * Speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.E)) {
+        if (Input.GetKey(KeyCode.E) && !BumpWallRight) {
             transform.Translate (Vector3.right * Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("WallLeft")) {
+            BumpWallLeft = true;
+        }
+        if (other.gameObject.CompareTag("WallRight")) {
+            BumpWallRight = true;
+        }
+        if (other.gameObject.CompareTag("Corner1")) {
+            transform.forward = Vector3.right;
+        }
+        if (other.gameObject.CompareTag("Corner2")) {
+            transform.forward = Vector3.back;
+        }
+    }    
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("WallLeft")) {
+            BumpWallLeft = false;
+        }
+        if (other.gameObject.CompareTag("WallRight")) {
+            BumpWallRight = false;
         }
     }
 }
